d26ba4fe3fdf51d49b9d41c45f41bb9e98f5affd
Gaurav Ganju <gauravganju@gmail.com>
2024-05-25 00:39:51 +0530

    Initial Commit

---
+++ PasswordManager/PasswordManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordManagerApp.swift
+//  PasswordManager
+//
+//  Created by Gaurav Ganju on 25/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PasswordManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
